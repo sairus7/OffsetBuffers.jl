a9d4eb309f07aac806225e11ef5faa9ef4d20f9c
Victor G. Gumennyy <20798349+sairus7@users.noreply.github.com>
2019-09-30 03:32:39 +0300

    Update buffers.jl

--- examples/buffers.jl
+++ examples/buffers.jl
@@ -15,5 +15,5 @@ end
 same_index = map(i-> buf[i] == alldata[i], eachindex(buf)) |> all
 
 if same_index
-    println("Buffer hold the same indexes for the last elenements of the original data array.")
+    println("Buffer hold last elenements of the original data array with the same indexes.")
 end
